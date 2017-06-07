b9dc7c5f0083570d9e4f0315a5cbb8a8d4f369ad
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-06-07 15:12:53 +0200

    use new Ordino wrapper

--- src/index.ts
+++ src/index.ts
@@ -11,37 +11,13 @@ import 'phovea_ui/src/_bootstrap';
 import 'phovea_ui/src/_font-awesome';
 import 'ordino/src/style.scss';
 import * as loginForm from 'html-loader!./_loginForm.html';
+import Ordino from 'ordino/src/Ordino';
 
-import * as template from 'phovea_clue/src/template';
-import * as header from 'phovea_ui/src/header';
-import {create} from 'ordino/src/Targid';
+
 import * as DockerName from 'docker-names';
 import {randomId} from 'phovea_core/src';
 
-// cache the nodes from the ordino/index.html before the TargID app is created
-// NOTE: the template (see next line) replaces the content of the document.body (but not document.head)
-const appNode = document.getElementById('app');
-const extrasNode = document.getElementById('extras');
-
-// cache targid instance for logo app link
-let ordino;
-
-// create TargID app from CLUE template
-const elems = template.create(document.body, {
-  app: 'Ordino',
-  appLink: new header.AppHeaderLink('Ordino', (event) => {
-    event.preventDefault();
-    ordino.openStartMenu();
-    return false;
-  }),
-  application: 'Ordino',
-  id: 'ordino',
-  recordSelectionTypes: null, // no automatic selection recording
-  provVisCollapsed: true,
-  thumbnails: false,
-  headerOptions: {
-    showReportBugLink: true
-  },
+new Ordino({
   loginForm: String(loginForm)
 });
 
@@ -52,22 +28,3 @@ const elems = template.create(document.body, {
   (<HTMLInputElement>document.querySelector('input#login_username')).value = username;
   (<HTMLInputElement>document.querySelector('input#login_password')).value = password;
 }
-
-const aboutDialogBody = elems.header.aboutDialog;
-aboutDialogBody.insertAdjacentHTML('afterbegin', '<div class="alert alert-warning" role="alert"><strong>Disclaimer</strong> This software is <strong>for research purpose only</strong>.</span></div>');
-
-
-// copy nodes from original document to new document (template)
-const mainNode = <HTMLElement>elems.$main.node();
-mainNode.classList.add('targid');
-while (appNode.firstChild) {
-  mainNode.appendChild(appNode.firstChild);
-}
-while (extrasNode.firstChild) {
-  document.body.appendChild(extrasNode.firstChild);
-}
-
-// create TargID app once the provenance graph is available
-elems.graph.then((graph) => {
-  ordino = create(graph, elems.clueManager, mainNode, elems);
-});
