d350873ff0d00ee09629e7b6a012ca93a7d50480
Holger Stitz <holger.stitz@jku.at>
2018-02-09 16:20:20 +0100

    Use MutationObserver for modification on about dialog Caleydo/tdp_bi_bioinfodb#646

--- src/index.ts
+++ src/index.ts
@@ -42,41 +42,68 @@ new Ordino({
 function injectDisclaimer(selector) {
   const alert = <HTMLElement>document.querySelector(selector);
   if (!alert) {
-    setTimeout(injectDisclaimer, 2000, selector); //wait another 2s
+    setTimeout(injectDisclaimer, 1000, selector); //wait another second
     return;
   }
   alert.innerHTML = `<strong>Disclaimer:</strong> This software is <strong>for research purpose and non-commercial use only</strong>.`;
 }
-setTimeout(injectDisclaimer, 10, '.welcomeView .disclaimer .alert');
+setTimeout(injectDisclaimer, 100, '.welcomeView .disclaimer .alert');
 
+
+// remove disclaimer alert from about dialog
+document.querySelector('#headerAboutDialog .modal-body .alert').remove();
+
+// insert Ordino Public about disclaimer
 const metaData = document.querySelector(`#headerAboutDialog .modal-body .metaData`);
 metaData.insertAdjacentHTML('beforebegin', `<article class="about-disclaimer">${aboutDisclaimer}</article>`);
+
 // move the information about caleydo to the source code section and remove the rest of the info
 const caleydoInfo = document.querySelector(`#headerAboutDialog .modal-body .caleydoInfo`);
 document.getElementById('about-source-code').insertAdjacentElement('beforeend', caleydoInfo.querySelector('p'));
 caleydoInfo.remove();
 
-// wait until the metadata is loaded, then move the version number remove the rest of the metadata
-function moveMetaDataVersion() {
-  const version = <HTMLElement>metaData.querySelector('p.version');
-  if (!version) {
-    setTimeout(moveMetaDataVersion, 2000); //wait another 2s
-    return;
-  }
-  document.getElementById('about-source-code').insertAdjacentElement('beforeend', version);
-  metaData.remove();
+
+/**
+ * Create a MutationObserver that checks for childList modifications
+ * @param selector
+ * @param {(mutation: MutationRecord, observer: MutationObserver) => void} callback
+ */
+function createMutationObserver(selector, callback: (mutation: MutationRecord, observer: MutationObserver) => void) {
+  const observer = new MutationObserver((mutationsList) => {
+    for(const mutation of mutationsList) {
+      if (mutation.type === 'childList') {
+        callback(mutation, observer);
+      }
+    }
+  });
+  observer.observe(document.querySelector(selector), { childList: true });
 }
 
-setTimeout(moveMetaDataVersion, 2000);
 
-// remove alert disclaimer from about dialog
-function removeDisclaimer(selector) {
-  const alert = <HTMLElement>document.querySelector(selector);
-  if (!alert) {
-    setTimeout(injectDisclaimer, 2000, selector); //wait another 2s
-    return;
-  }
-  alert.remove();
+/**
+ * wait until the metadata is loaded, then move the version number remove the rest of the metadata
+ * @param {MutationRecord} mutation
+ * @param {MutationObserver} observer
+ */
+function moveMetaDataVersion(mutation: MutationRecord, observer: MutationObserver) {
+  observer.disconnect(); // stop listening
+  const target = <HTMLElement>mutation.target;
+  document.getElementById('about-source-code' ).insertAdjacentElement('beforeend', target.querySelector('.version'));
+  target.remove();
 }
+createMutationObserver(`#headerAboutDialog .modal-body .metaData`, moveMetaDataVersion);
 
-setTimeout(removeDisclaimer, 2000, '#headerAboutDialog .modal-body .alert');
+
+/**
+ * Change about dialog title to `Ordino` (instead of `Ordino Public`)
+ * @param {MutationRecord} mutation
+ * @param {MutationObserver} observer
+ */
+function modifyDialogTitle(mutation: MutationRecord, observer: MutationObserver) {
+  observer.disconnect(); // stop listening
+  if(mutation.target.textContent !== 'Ordino') {
+    mutation.target.textContent = 'Ordino';
+  }
+}
+createMutationObserver(`#headerAboutDialog .modal-title`, modifyDialogTitle);
+
