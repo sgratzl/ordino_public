a3fa7babbe88a36bfacb44bc1cda9fbd237254cf
Holger Stitz <holger.stitz@jku.at>
2018-04-16 16:57:07 +0200

    Remove d3 dependency for welcome view Caleydo/tdp_bi_bioinfodb#840

--- src/WelcomeView.ts
+++ src/WelcomeView.ts
@@ -1,16 +1,14 @@
-import * as d3 from 'd3';
 import {IWelcomeView} from 'ordino/src/WelcomeView';
 
 import WelcomeViewTemplate from 'html-loader!./welcome_view.html';
 
 export default class WelcomeView implements IWelcomeView {
-  private $node: d3.Selection<any>;
 
-  constructor(parent: HTMLElement) {
-    this.$node = d3.select(parent);
+  constructor(private parent: HTMLElement) {
+    //
   }
 
   build() {
-    this.$node.html(WelcomeViewTemplate);
+    this.parent.innerHTML = WelcomeViewTemplate;
   }
 }
