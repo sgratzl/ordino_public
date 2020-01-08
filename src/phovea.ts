472a9901dfa6d6f9ab2c8e15a1825a7b034f3c85
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-01-08 18:04:44 +0100

    change import statements

--- src/phovea.ts
+++ src/phovea.ts
@@ -6,12 +6,12 @@ import {ILocaleEPDesc, EP_PHOVEA_CORE_LOCALE} from 'phovea_core/src/extensions';
 export default function (registry: IRegistry) {
   //registry.push('extension-type', 'extension-id', function() { return import('./extension_impl'); }, {});
   // generator-phovea:begin
-  registry.push('ordinoWelcomeView', 'ordinoPublicWelcomeView', function () {return System.import('ordino_public/src/WelcomeView.ts');}, {
+  registry.push('ordinoWelcomeView', 'ordinoPublicWelcomeView', function () {return System.import('./WelcomeView.ts');}, {
     priority: 20
   });
 
   registry.push(EP_PHOVEA_CORE_LOCALE, 'ordinoPublicLocaleEN', function () {
-    return System.import('ordino_public/src/assets/locales/en/ordino_public.json').then(asResource);
+    return System.import('./assets/locales/en/ordino_public.json').then(asResource);
   }, <ILocaleEPDesc>{
     ns: 'ordino_public',
   });
