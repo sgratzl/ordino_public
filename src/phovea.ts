8075087c9cfd1601db04983b5699bc9c21224b5b
Holger Stitz <holger.stitz@datavisyn.io>
2020-01-10 11:09:24 +0100

    Switch locale from ordino_public to tdp  namespace

--- src/phovea.ts
+++ src/phovea.ts
@@ -10,10 +10,10 @@ export default function (registry: IRegistry) {
     priority: 20
   });
 
-  registry.push(EP_PHOVEA_CORE_LOCALE, 'ordinoPublicLocaleEN', function () {
-    return System.import('./assets/locales/en/ordino_public.json').then(asResource);
+  registry.push(EP_PHOVEA_CORE_LOCALE, 'ordinoPublicTdpLocaleEN', function () {
+    return System.import('./assets/locales/en/tdp.json').then(asResource);
   }, <ILocaleEPDesc>{
-    ns: 'ordino_public',
+    ns: 'tdp',
   });
 
   // generator-phovea:end
