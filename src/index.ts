d1102a48cf02062b8ba361dfb03c1cbb2fd0ee54
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-05-14 18:42:18 +0200

    fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,4 @@
-/********************************************************************
- * Copyright (c) The Caleydo Team, http://caleydo.org
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- ********************************************************************/
-
-
-// Determine the order of css files manually
-
-import 'file-loader?name=index.html!extract-loader!html-loader?interpolate!ordino/src/index.html';
-import 'file-loader?name=404.html!ordino/src/404.html';
-import 'file-loader?name=robots.txt!ordino/src/robots.txt';
-import 'ordino/src/style.scss';
-import * as aboutDisclaimer from 'html-loader!./_aboutDisclaimer.html';
-import Ordino from 'ordino/src/Ordino';
-import './style.scss';
-import getMetaData from 'phovea_ui/src/metaData';
-
-const _ = new Ordino({
-  showCookieDisclaimer: true,
-  showResearchDisclaimer: false,
-  showAboutLink
-});
-
-function showAboutLink(title: HTMLElement, content: HTMLElement) {
-  title.innerHTML = 'Ordino';
-  // insert Ordino Public about disclaimer
-  const caleydoInfo = content.querySelector(`.caleydoInfo p`);
-  content.innerHTML = `<article class="about-disclaimer">${aboutDisclaimer}</article>`;
-  // move the information about caleydo to the source code section and remove the rest of the info
-  document.getElementById('about-source-code').insertAdjacentElement('beforeend', caleydoInfo);
-
-  getMetaData().then((metaData) => {
-    document.getElementById('about-source-code').insertAdjacentHTML('beforeend', `<p class="version"><strong>Version</strong>: ${metaData.version}</p>`);
-  });
-}
+export * from './app';
+export {default} from './phovea';
+export * from './WelcomeView';
+export {default as WelcomeView} from './WelcomeView';
