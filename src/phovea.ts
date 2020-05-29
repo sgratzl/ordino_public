e6476c97104b92d96d16228363d457bc95fd92b4
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-05-29 09:29:21 +0200

    remove System.import()

--- src/phovea.ts
+++ src/phovea.ts
@@ -6,12 +6,12 @@ import {ILocaleEPDesc, EP_PHOVEA_CORE_LOCALE} from 'phovea_core/src/extensions';
 export default function (registry: IRegistry) {
   //registry.push('extension-type', 'extension-id', function() { return import('./extension_impl'); }, {});
   // generator-phovea:begin
-  registry.push('ordinoWelcomeView', 'ordinoPublicWelcomeView', function () {return System.import('./WelcomeView.ts');}, {
+  registry.push('ordinoWelcomeView', 'ordinoPublicWelcomeView', function () {return import('./WelcomeView.ts');}, {
     priority: 20
   });
 
   registry.push(EP_PHOVEA_CORE_LOCALE, 'ordinoPublicTdpLocaleEN', function () {
-    return System.import('./assets/locales/en/tdp.json').then(asResource);
+    return import('./assets/locales/en/tdp.json').then(asResource);
   }, <ILocaleEPDesc>{
     ns: 'tdp',
   });
