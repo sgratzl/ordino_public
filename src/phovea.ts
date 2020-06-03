1b7529364a98d561b8695e2217551c1d8e160ce1
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-06-03 14:23:33 +0200

    fix dynamic import in phovea.ts

--- src/phovea.ts
+++ src/phovea.ts
@@ -4,7 +4,7 @@ import {ILocaleEPDesc, EP_PHOVEA_CORE_LOCALE} from 'phovea_core';
 export default function (registry: IRegistry) {
   //registry.push('extension-type', 'extension-id', function() { return import('./extension_impl'); }, {});
   // generator-phovea:begin
-  registry.push('ordinoWelcomeView', 'ordinoPublicWelcomeView', function () {return import('./app/WelcomeView');}, {
+  registry.push('ordinoWelcomeView', 'ordinoPublicWelcomeView', function () {return import('./app/WelcomeView').then((w) => w.WelcomeView);}, {
     priority: 20
   });
 
