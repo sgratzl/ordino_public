cf7251f72f19ef84c3e38465fd5805836d24571b
oltionchampari <oltion.champari@datavisyn.io>
2020-07-08 11:58:16 +0200

    Add persistent sessions message to ordino_public
    Caleydo/ordino_public#62

--- src/WelcomeView.ts
+++ src/WelcomeView.ts
@@ -10,5 +10,15 @@ export default class WelcomeView implements IWelcomeView {
 
   build() {
     this.parent.insertAdjacentHTML('afterbegin', WelcomeViewTemplate);
+
+    // add message to ordino public - temporary change
+    const template = `
+    <div class="alert alert-warning" style="max-width: fit-content;" role="alert">
+      <strong>Please note:</strong> We have upgraded Ordino to the hg38 genome build, which might change the result of some old sessions.
+      If you experience some issues please open previous sessions in the <a href="https://ordino-hg19.caleydoapp.org">Ordino with hg19</a>.
+    </div>`;
+
+    const persitedSessionsNode = document.querySelector('.tdpSessionPersistentData .item');
+    persitedSessionsNode.insertAdjacentHTML('afterbegin', template);
   }
 }
