9fcb5c1c287d0392fda170f2251ab2ba2defc11b
oltionchampari <oltion.champari@datavisyn.io>
2021-03-09 07:28:05 +0100

    Add npm script

---
+++ dist/clientConfig.d.ts
@@ -0,0 +1,16 @@
+interface IHeaderOptions {
+    color: 'black' | 'white';
+    size: 'sm' | 'md' | 'lg';
+}
+export default interface IClientConfigOptions {
+    /**
+     * @description to enable vs codes
+     */
+    enableDatasetsTab?: boolean;
+    enableToursTab?: boolean;
+    enableSessionsTab?: boolean;
+    enableMoreTab?: boolean;
+    header: IHeaderOptions;
+    extra: string;
+}
+export {};
