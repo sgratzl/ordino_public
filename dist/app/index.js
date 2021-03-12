b2647e01501085e39de4dee83ee117b260fca7f7
Holger Stitz <holger.stitz@datavisyn.io>
2021-03-12 20:58:01 +0100

    Remove welcome view

--- dist/app/index.js
+++ dist/app/index.js
@@ -1,2 +1,1 @@
-export * from './WelcomeView';
 //# sourceMappingURL=index.js.map
--- dist/phovea.js
+++ dist/phovea.js
@@ -1,20 +1,12 @@
 import { PluginRegistry } from 'phovea_core';
 import { EP_PHOVEA_CORE_LOCALE } from 'phovea_core';
-import { EP_PHOVEA_CLUE_PROVENANCE_GRAPH } from 'phovea_clue';
 export default function (registry) {
     //registry.push('extension-type', 'extension-id', function() { return import('./extension_impl'); }, {});
     // generator-phovea:begin
-    registry.push('ordinoWelcomeView', 'ordinoPublicWelcomeView', function () { return import('./app/WelcomeView'); }, {
-        factory: 'new WelcomeView',
-        priority: 20
-    });
     registry.push(EP_PHOVEA_CORE_LOCALE, 'ordinoPublicTdpLocaleEN', function () {
         return import('./locales/en/tdp.json').then(PluginRegistry.getInstance().asResource);
     }, {
         ns: 'tdp',
-    });
-    registry.push(EP_PHOVEA_CLUE_PROVENANCE_GRAPH, 'dismissMigrationPopup', () => import('./app/WelcomeView'), {
-        factory: 'dismissMigrationPopup'
     });
     // generator-phovea:end
 }
