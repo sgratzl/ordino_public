0ce6c69bb35f0af7dabfc558d23550969dde0ea8
oltionchampari <oltion.champari@datavisyn.io>
2021-03-16 10:29:16 +0100

    Add links to dataset page
    Caleydo/tdp_bi_bioinfodb#1197

--- src/pages/DatasetsPage.tsx
+++ src/pages/DatasetsPage.tsx
@@ -4,14 +4,19 @@ import {OrdinoFooter} from './components/OrdinoFooter';
 import {Row, Col, Card, Container} from 'react-bootstrap';
 
 
-export function DatasetCard({title, text}: ICardProps) {
+
+interface IDatasetPageCardProps {
+  title: string;
+  children?: React.ReactNode;
+}
+
+
+export function DatasetPageCard({title, children}: IDatasetPageCardProps) {
   return (
     <Card style={{height: '100%'}} className="shadow-sm">
       <Card.Body>
         <Card.Title>{title}</Card.Title>
-        <Card.Text>
-          {text}
-        </Card.Text>
+        {children}
       </Card.Body>
     </Card>
   );
@@ -30,41 +35,43 @@ export function DatasetsPage() {
         </Row>
         <Row xs={1} md={2} >
           <Col>
-            <DatasetCard title="The Cancer Genome Atlas (TCGA)"
-              text={`Gene expression, mutation, and copy number data cancergenome.nih.gov`} />
+            <DatasetPageCard title="The Cancer Genome Atlas (TCGA)">
+              <Card.Text>Gene expression, mutation, and copy number data </Card.Text>
+              <Card.Link href="https://cancergenome.nih.gov" target="_blank" rel="noopener">cancergenome.nih.gov</Card.Link>
+            </DatasetPageCard>
           </Col>
           <Col>
-            <DatasetCard title="Cancer Cell Line Encyclopedia (CCLE)"
-              text="Gene expression, mutation, and copy number data
-            portals.broadinstitute.org/ccle" />
+            <DatasetPageCard title="Cancer Cell Line Encyclopedia (CCLE)">
+              <Card.Text>Gene expression, mutation, and copy number data</Card.Text>
+              <Card.Link href="https://portals.broadinstitute.org/ccle" target="_blank" rel="noopener">portals.broadinstitute.org/ccle</Card.Link>
+            </DatasetPageCard>
           </Col>
         </Row>
         <Row className="mt-4">
           <Col>
-            <h4 className="text-left d-flex align-items-center mt-2 mb-3"><i className="mr-2 fas ordino-icon-1 fa-chevron-circle-right" ></i> Depletion sceen data</h4>
+            <h4 className="text-left d-flex align-items-center mt-2 mb-3"><i className="mr-2 fas ordino-icon-1 fa-chevron-circle-right" ></i> Depletion Sceen Data</h4>
           </Col>
         </Row>
         <Row xs={1} md={2} >
           <Col>
-            <DatasetCard title="Project DRIVE"
-              text="RNAi depletion screen data (RSA and ATARiS)
-            McDonald III, E. R. et. al. Project DRIVE: A Compen- dium of Cancer Dependencies and Synthetic Lethal Relationships Uncovered by Large-Scale, Deep RNAi Screening.
-             Cell 170, Pages 577-592.e10 (2017)." />
+            <DatasetPageCard title="Project DRIVE">
+
+              <Card.Text>RNAi depletion screen data (RSA and ATARiS)</Card.Text>
+              <Card.Link href="https://doi.org/10.1016/j.cell.2017.07.005" target="_blank" rel="noopener"> McDonald III, E. R. et. al.
+              Project DRIVE: A Compen- dium of Cancer Dependencies and Synthetic Lethal Relationships Uncovered by Large-Scale, Deep RNAi Screening.
+             Cell 170, Pages 577-592.e10 (2017).</Card.Link>
+            </DatasetPageCard>
           </Col>
           <Col>
-            <DatasetCard title="Avana CERES"
-              text="CRISPR-Cas9 depletion screen data
-            Meyers, R. M. et. al. Computational correction of copy
-             number effect improves specificity of CRISPR–Cas9 essentiality screens in cancer cells. Nature Genetics 49, 1779–1784 (2017)." />
+            <DatasetPageCard title="Avana CERES">
+              <Card.Text>CRISPR-Cas9 depletion screen data</Card.Text>
+              <Card.Link href="https://doi.org/10.1038/ng.3984" target="_blank" rel="noopener">Meyers, R. M. et. al. Computational correction of copy
+             number effect improves specificity of CRISPR–Cas9 essentiality screens in cancer cells. Nature Genetics 49, 1779–1784 (2017).</Card.Link>
+            </DatasetPageCard>
           </Col>
         </Row>
       </Container>
       <OrdinoFooter></OrdinoFooter>
     </>
   );
-}
-
-interface ICardProps {
-  title: string;
-  text: string;
 }
