454501ea6ed4c5a7f2456a216baf8e970e92f205
Holger Stitz <holger.stitz@datavisyn.io>
2021-04-08 16:22:58 +0200

    Launch tours in app

--- dist/pages/ToursPage.js
+++ dist/pages/ToursPage.js
@@ -12,13 +12,13 @@ export function ToursPage() {
                 React.createElement("i", { className: "mr-2 ordino-icon-1 fas fa-chevron-circle-right" }),
                 " Beginner"),
             React.createElement(Row, { className: "mb-4", md: 3 },
-                React.createElement(TourCard, { title: "Ordino Welcome Tour", text: "Learn the basic features of Ordino in a short welcome tour.", image: tour1Img, onClickHandler: (evt) => console.log('') }),
-                React.createElement(TourCard, { title: "Overview of Start Menu", text: "This tour provides an overview of the Ordino start menu.", image: tour1Img, onClickHandler: (evt) => console.log('') })),
+                React.createElement(TourCard, { title: "Ordino Welcome Tour", text: "Learn the basic features of Ordino in a short welcome tour.", image: tour1Img, href: "/app/#tour=ordinoWelcomeTour" }),
+                React.createElement(TourCard, { title: "Overview of Start Menu", text: "This tour provides an overview of the Ordino start menu.", image: tour1Img, href: "/app/#tour=ordinoStartMenuTour" })),
             React.createElement("h4", { className: "text-left mt-4 mb-3 d-flex align-items-center " },
                 React.createElement("i", { className: "mr-2 ordino-icon-1 fas fa-chevron-circle-right" }),
                 " Advanced"),
             React.createElement(Row, { md: 3 },
-                React.createElement(TourCard, { title: "Adding data Columns", text: "Learn how to add data columns to rankings in Ordino.", image: tour1Img, onClickHandler: (evt) => console.log('') }))),
+                React.createElement(TourCard, { title: "Adding data Columns", text: "Learn how to add data columns to rankings in Ordino.", image: tour1Img, href: "/app/#tour=ordinoAddColumnToGeneListTour" }))),
         React.createElement(OrdinoFooter, { openInNewWindow: true })));
 }
 //# sourceMappingURL=ToursPage.js.map
