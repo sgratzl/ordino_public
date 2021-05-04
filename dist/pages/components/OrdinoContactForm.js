4cc5401d9da3d96eed87220f252b367f3f8ab5ae
oltionchampari <oltion.champari@datavisyn.io>
2021-05-04 16:20:33 +0200

    Add contact form logic
    Caleydo/tdp_bi_bioinfodb#1222

--- dist/pages/components/OrdinoContactForm.js
+++ dist/pages/components/OrdinoContactForm.js
@@ -1,32 +1,62 @@
 import * as React from 'react';
-import { Col, Form, Button, Card } from 'react-bootstrap';
+import { Col, Form, Card } from 'react-bootstrap';
+const CONTACT_FORM_EMAIL = 'ordino@caleydo.org';
+const useSubmitContactForm = () => {
+    const [inputs, setInputs] = React.useState({
+        name: '',
+        subject: 'I have a question',
+        message: '',
+        email: '',
+        mailTo: 'mailto:' + CONTACT_FORM_EMAIL,
+    });
+    const handleInputChange = (event) => {
+        // persist event in order to access `event.target.name`
+        event.persist();
+        const { subject, message, name, email } = inputs;
+        let extra = subject || message ? '?' : '';
+        if (subject) {
+            extra += `subject=${encodeURIComponent(subject)}`;
+        }
+        if (message) {
+            // append name and email to the email body
+            // maybe it would be less confusing to completely remove these two fields and the user can add them directly to the message box
+            const body = `${message}${name ? '\n Name: ' + name : ''}${email ? '\n Email: ' + email : ''}`;
+            extra += `${subject ? '&' : ''}body=${encodeURIComponent(body)}`;
+        }
+        const newMailTo = 'mailto:' + CONTACT_FORM_EMAIL + extra;
+        setInputs((inputs) => ({ ...inputs, [event.target.name]: event.target.value, mailTo: newMailTo }));
+    };
+    return {
+        inputs,
+        handleInputChange,
+    };
+};
 export function OrdinoContactForm() {
-    // TODO: implemment backend
-    const handleSubmit = () => null;
+    const { inputs, handleInputChange } = useSubmitContactForm();
+    const { name, email, subject, message, mailTo } = inputs;
     return (React.createElement(Card, { className: "shadow-sm" },
         React.createElement(Card.Body, null,
             React.createElement(Card.Text, null,
-                'Do you have questions or found a bug, do not hasitate to contact us using the contact form below. You can also contact us by writing an email to ',
+                'Do you have questions or found a bug, do not hesitate to contact us using the contact form below. You can also contact us by writing an email to ',
                 React.createElement(Card.Link, { href: "mailto:ordino@caleydo.org." }, "ordino@caleydo.org"),
-                " or posting a message in the Microsoft Team. We are glad to help you."),
-            React.createElement(Form, { onSubmit: () => handleSubmit() },
+                ". We are glad to help you."),
+            React.createElement(Form, null,
                 React.createElement(Form.Group, { className: "row-cols-md-3" },
                     React.createElement(Form.Label, null, "Type of contact"),
-                    React.createElement(Form.Control, { as: "select" },
-                        React.createElement("option", null, "I have a question"),
-                        React.createElement("option", null, "I want to report a bug"),
-                        React.createElement("option", null, "Placeholder"))),
+                    React.createElement(Form.Control, { onChange: handleInputChange, value: subject, name: "subject", as: "select" },
+                        React.createElement("option", { value: "I have a question" }, "I have a question"),
+                        React.createElement("option", { value: "I want to report a bug" }, "I want to report a bug"))),
                 React.createElement(Form.Group, null,
                     React.createElement(Form.Label, null, "Message"),
-                    React.createElement(Form.Control, { as: "textarea", rows: 5 })),
+                    React.createElement(Form.Control, { as: "textarea", name: "message", value: message, rows: 5, onChange: handleInputChange })),
                 React.createElement(Form.Group, null,
                     React.createElement(Form.Label, null, "Name"),
-                    React.createElement(Form.Control, { type: "text", placeholder: "Name" })),
+                    React.createElement(Form.Control, { value: name, onChange: handleInputChange, name: "name", type: "text", placeholder: "Name" })),
                 React.createElement(Form.Group, null,
                     React.createElement(Form.Label, null, "Email address"),
-                    React.createElement(Form.Control, { type: "email", placeholder: "name@example.com" })),
+                    React.createElement(Form.Control, { type: "email", value: email, name: "email", placeholder: "name@example.com", onChange: handleInputChange })),
                 React.createElement(Form.Row, { className: "justify-content-end" },
                     React.createElement(Col, { md: 'auto' },
-                        React.createElement(Button, { variant: "secondary", type: "submit" }, "Send Message")))))));
+                        React.createElement("a", { href: mailTo, title: "Send Message", className: "btn btn-secondary" }, "Send Message")))))));
 }
 //# sourceMappingURL=OrdinoContactForm.js.map
--- src/pages/components/OrdinoContactForm.tsx
+++ src/pages/components/OrdinoContactForm.tsx
@@ -1,47 +1,89 @@
 import * as React from 'react';
-import {Col, Form, Button, Card} from 'react-bootstrap';
+import {Col, Form, Card} from 'react-bootstrap';
 
-export function OrdinoContactForm () {
-    // TODO: implemment backend
-    const handleSubmit = () => null;
+interface IOrdinoContactFormState {
+    name: string;
+    email?: string;
+    subject: string;
+    message: string;
+    mailTo: string;
+}
+
+const CONTACT_FORM_EMAIL = 'ordino@caleydo.org';
+
+const useSubmitContactForm = () => {
+    const [inputs, setInputs] = React.useState<IOrdinoContactFormState>({
+        name: '',
+        subject: 'I have a question',
+        message: '',
+        email: '',
+        mailTo: 'mailto:' + CONTACT_FORM_EMAIL,
+    });
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        // persist event in order to access `event.target.name`
+        event.persist();
+        const {subject, message, name, email} = inputs;
+        let extra = subject || message ? '?' : '';
+        if (subject) {
+            extra += `subject=${encodeURIComponent(subject)}`;
+        }
+        if (message) {
+            // append name and email to the email body
+            // maybe it would be less confusing to completely remove these two fields and the user can add them directly to the message box
+            const body = `${message}${name ? '\n Name: ' + name : ''}${email ? '\n Email: ' + email : ''}`;
+            extra += `${subject ? '&' : ''}body=${encodeURIComponent(body)}`;
+        }
+        const newMailTo = 'mailto:' + CONTACT_FORM_EMAIL + extra;
+        setInputs((inputs) => ({...inputs, [event.target.name]: event.target.value, mailTo: newMailTo}));
+    };
+    return {
+        inputs,
+        handleInputChange,
+    };
+};
+
+export function OrdinoContactForm() {
+
+    const {inputs, handleInputChange} = useSubmitContactForm();
+    const {name, email, subject, message, mailTo} = inputs;
 
     return (
         <Card className="shadow-sm">
             <Card.Body>
                 <Card.Text>
-                    {'Do you have questions or found a bug, do not hasitate to contact us using the contact form below. You can also contact us by writing an email to '}
-                    <Card.Link href="mailto:ordino@caleydo.org.">ordino@caleydo.org</Card.Link> or posting a message in the Microsoft Team. We are glad to help you.
+                    {'Do you have questions or found a bug, do not hesitate to contact us using the contact form below. You can also contact us by writing an email to '}
+                    <Card.Link href="mailto:ordino@caleydo.org.">ordino@caleydo.org</Card.Link>. We are glad to help you.
                  </Card.Text>
-                <Form onSubmit={() => handleSubmit()}>
+                <Form>
                     <Form.Group className="row-cols-md-3">
                         <Form.Label>Type of contact</Form.Label>
-                        <Form.Control as="select">
-                            <option>I have a question</option>
-                            <option>I want to report a bug</option>
-                            <option>Placeholder</option>
+                        <Form.Control onChange={handleInputChange} value={subject} name="subject" as="select">
+                            <option value="I have a question">I have a question</option>
+                            <option value="I want to report a bug">I want to report a bug</option>
                         </Form.Control>
                     </Form.Group>
 
                     <Form.Group>
                         <Form.Label>Message</Form.Label>
-                        <Form.Control as="textarea" rows={5} />
+                        <Form.Control as="textarea" name="message" value={message} rows={5} onChange={handleInputChange}>
+                        </Form.Control>
                     </Form.Group>
 
                     <Form.Group >
                         <Form.Label>Name</Form.Label>
-                        <Form.Control type="text" placeholder="Name" />
+                        <Form.Control value={name} onChange={handleInputChange} name="name" type="text" placeholder="Name" />
                     </Form.Group>
 
                     <Form.Group >
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="name@example.com" />
+                        <Form.Control type="email" value={email} name="email" placeholder="name@example.com" onChange={handleInputChange} />
                     </Form.Group>
 
                     <Form.Row className="justify-content-end">
                         <Col md={'auto'}>
-                            <Button variant="secondary" type="submit">
+                            <a href={mailTo} title="Send Message" className="btn btn-secondary">
                                 Send Message
-                            </Button>
+                            </a>
                         </Col>
                     </Form.Row>
                 </Form>
