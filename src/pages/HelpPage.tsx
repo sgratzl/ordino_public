d57f54c4297442f315c26f13f7d645b262dbb046
Holger Stitz <holger.stitz@datavisyn.io>
2021-05-14 23:19:19 +0200

    Use `useScrollToSlug` hook on help page

--- src/pages/HelpPage.tsx
+++ src/pages/HelpPage.tsx
@@ -2,9 +2,11 @@ import * as React from 'react';
 import {HeaderNavigation, OrdinoFooter} from 'ordino';
 import {DevelopedByAffiliations} from './components/DevelopedByAffiliations';
 import {OrdinoHelpSection} from './components/OrdinoHelpSection';
-
+import {useScrollToSlug} from 'ordino';
 
 export function HelpPage() {
+  useScrollToSlug();
+
   return (
     <>
       <HeaderNavigation fixed="top" />
