b8f14db66bcd096cdd1a19888483ad0b3a819683
Holger Stitz <holger.stitz@datavisyn.io>
2021-05-19 18:52:53 +0200

    Add status indicator while loading the version

--- src/pages/components/SourceCodeCard.tsx
+++ src/pages/components/SourceCodeCard.tsx
@@ -10,23 +10,21 @@ export function SourceCodeCard() {
 
   return (
     <>
-      {status === 'success' &&
-        <div className="card shadow-sm">
-          <div className="card-body">
-            <p className="card-text">
-              The source code of Ordino is available under the Mozilla Public
-              License (MPL) at GitHub.
-        </p>
-            <p className="card-text">
-              This application is part of Phovea, a platform for developing
-              web-based visualization applications. For tutorials, API docs, and
-              more information about the build and deployment process, see the
-              documentation page.
-        </p>
-
-            <p className="card-text">{`Version: ${value.version}`}</p>
-          </div>
+      <div className="card shadow-sm">
+        <div className="card-body">
+          <p className="card-text">
+            The source code of Ordino is available under the Mozilla Public
+            License (MPL) at GitHub.
+          </p>
+          <p className="card-text">
+            This application is part of Phovea, a platform for developing
+            web-based visualization applications. For tutorials, API docs, and
+            more information about the build and deployment process, see the
+            documentation page.
+          </p>
+          <p className="card-text"><b>Version: </b> {(status === 'success') ? value.version : 'Fetching current version ...'}</p>
         </div>
-      }</>
+      </div>
+    </>
   );
 }
