64ae78b54bcafecccd5c9925af67de14ecda3450
oltionchampari <oltion.champari@datavisyn.io>
2021-05-19 17:17:14 +0200

    Show correct version in SourceCodeCard
    Caleydo/tdp_bi_bioinfodb#1227

--- src/pages/components/SourceCodeCard.tsx
+++ src/pages/components/SourceCodeCard.tsx
@@ -1,21 +1,32 @@
+
+
+import {useAsync} from 'ordino';
+import {AppMetaDataUtils} from 'phovea_ui';
 import React from 'react';
 
 export function SourceCodeCard() {
+  const loadMetaData = React.useMemo(() => () => AppMetaDataUtils.getMetaData(), []);
+  const {status, value} = useAsync(loadMetaData);
+
   return (
-    <div className="card shadow-sm">
-      <div className="card-body">
-        <p className="card-text">
-          The source code of Ordino is available under the Mozilla Public
-          License (MPL) at GitHub.
+    <>
+      {status === 'success' &&
+        <div className="card shadow-sm">
+          <div className="card-body">
+            <p className="card-text">
+              The source code of Ordino is available under the Mozilla Public
+              License (MPL) at GitHub.
         </p>
-        <p className="card-text">
-          This application is part of Phovea, a platform for developing
-          web-based visualization applications. For tutorials, API docs, and
-          more information about the build and deployment process, see the
-          documentation page.
+            <p className="card-text">
+              This application is part of Phovea, a platform for developing
+              web-based visualization applications. For tutorials, API docs, and
+              more information about the build and deployment process, see the
+              documentation page.
         </p>
-        <p className="card-text">Version: 7.0.3-20201119-011617</p>
-      </div>
-    </div>
+
+            <p className="card-text">{`Version: ${value.version}`}</p>
+          </div>
+        </div>
+      }</>
   );
 }
