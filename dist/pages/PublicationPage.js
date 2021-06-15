118fe94d1ef51703a5b3945d5eb57ef1e18f443b
Holger Stitz <holger.stitz@datavisyn.io>
2021-06-15 23:47:09 +0200

    Integrate feedback for publication page
    https://github.com/Caleydo/tdp_bi_bioinfodb/issues/1244

--- dist/pages/PublicationPage.js
+++ dist/pages/PublicationPage.js
@@ -1,17 +1,17 @@
 import * as React from 'react';
 import { HeaderNavigation, OrdinoFooter, OrdinoScrollspy, OrdinoScrollspyItem } from 'ordino';
-// tslint:disable-next-line: variable-name
-const OrdinoPublication = () => {
+function OrdinoPublication() {
     return React.createElement("div", { className: "row" },
         React.createElement("div", { className: "shadow-sm card p-2 overflow-hidden" },
             React.createElement("div", { className: "card-body" },
                 React.createElement("iframe", { className: "mb-2", width: "100%", height: "100%", src: "https://www.youtube-nocookie.com/embed/TIDUsEOsI_Y?autoplay=0", frameBorder: "0", allow: "autoplay; clipboard-write; encrypted-media; picture-in-picture", allowFullScreen: true }),
+                React.createElement("h5", { className: "card-title" }, "Abstract"),
                 React.createElement("p", { className: "card-text" }, "Ordino is a web-based analysis tool for cancer genomics that allows users to flexibly rank, filter and explore genes, cell lines and tissue samples based on pre-loaded data, including The Cancer Genome Atlas, the Cancer Cell Line Encyclopedia and manually uploaded information. Interactive tabular data visualization that facilitates the user-driven prioritization process forms a core component of Ordino. Detail views of selected items complement the exploration. Findings can be stored, shared and reproduced via the integrated session management."),
-                React.createElement("p", { className: "card-text" }, "Ordino was published in the Oxford Bioinformatics journal. Please consider a citation when using Ordino and publishing your results."),
-                React.createElement("p", { className: "text-muted card-text" },
-                    "Marc Streit, Samuel Gratzl, Holger Stitz, Andreas Wernitznig, Thomas Zichner, Christian Haslinger",
+                React.createElement("h5", { className: "card-title" }, "Citation"),
+                React.createElement("p", { className: "card-text" },
+                    "Marc Streit, Samuel Gratzl, Holger Stitz, Andreas Wernitznig, Thomas Zichner, Christian Haslinger.",
                     React.createElement("br", null),
-                    React.createElement("b", null, "Ordino: visual analysis tool for ranking and exploring genes, cell lines, and tissue samples"),
+                    React.createElement("b", null, "Ordino: visual analysis tool for ranking and exploring genes, cell lines, and tissue samples."),
                     React.createElement("br", null),
                     React.createElement("i", null, "Bioinformatics, 35(17): 3140-3142, 2019."))),
             React.createElement("div", { className: "card-footer" },
@@ -24,16 +24,17 @@ const OrdinoPublication = () => {
                 React.createElement("a", { href: "https://github.com/Caleydo/ordino_public", target: "_blank", rel: "noopener noreferrer", className: "btn btn-light" },
                     React.createElement("i", { className: "mr-1 fab fa-github" }),
                     " Source code"))));
-};
-// tslint:disable-next-line: variable-name
-const TourdinoPublication = () => {
+}
+;
+function TourdinoPublication() {
     return React.createElement("div", { className: "row" },
         React.createElement("div", { className: "shadow-sm card p-2 overflow-hidden" },
             React.createElement("div", { className: "card-body" },
                 React.createElement("iframe", { className: "mb-2", width: "100%", height: "100%", src: "https://www.youtube-nocookie.com/embed/k6EPm6i-Vw4?autoplay=0", frameBorder: "0", allow: "autoplay; clipboard-write; encrypted-media; picture-in-picture", allowFullScreen: true }),
+                React.createElement("h5", { className: "card-title" }, "Abstract"),
                 React.createElement("p", { className: "card-text" }, "Seeking relationships and patterns in tabular data is a common data exploration task. To confirm hypotheses that are based on visual patterns observed during exploratory data analysis, users need to be able to quickly compare data subsets, and get further information on the significance of the result and the statistical test applied. Existing tools, however, either focus on the comparison of a single data type, such as comparing numerical attributes only, or provide little or no statistical evaluation to assess a hypothesis. To fill this gap, we present TourDino, a support view that helps users who are not experts in statistics to verify generated hypotheses and confirm insights gained during the exploration of tabular data. In TourDino we present an overview of the statistical significance of various row or column comparisons. On demand, we show further details, including the test score, a textual description, and a detail visualization explaining the results. To demonstrate the efficacy of our approach, we have integrated TourDino in the Ordino drug discovery platform for the purpose of identifying new drug targets."),
-                React.createElement("p", { className: "card-text" }, "Tourdino was presented at the EuroVis Workshop on Visual Analytics. Please consider a citation when using Ordino and publishing your results."),
-                React.createElement("p", { className: "card-text text-muted" },
+                React.createElement("h5", { className: "card-title" }, "Citation"),
+                React.createElement("p", { className: "card-text" },
                     "Klaus Eckelt, Patrick Adelberger, Thomas Zichner, Andreas Wernitznig, Marc Streit.",
                     React.createElement("br", null),
                     React.createElement("b", null, "TourDino: A Support View for Confirming Patterns in Tabular Data."),
@@ -49,16 +50,17 @@ const TourdinoPublication = () => {
                 React.createElement("a", { href: "https://github.com/Caleydo/tourdino", target: "_blank", rel: "noopener noreferrer", className: "btn btn-light" },
                     React.createElement("i", { className: "mr-1 fab fa-github" }),
                     " Source code"))));
-};
-// tslint:disable-next-line: variable-name
-const TagglePublication = () => {
+}
+;
+function TagglePublication() {
     return React.createElement("div", { className: "row" },
         React.createElement("div", { className: "shadow-sm card p-2 overflow-hidden" },
             React.createElement("div", { className: "card-body" },
                 React.createElement("iframe", { className: "mb-2", width: "100%", height: "100%", src: "https://www.youtube-nocookie.com/embed/t50KgQKK8EQ?autoplay=0", frameBorder: "0", allow: "autoplay; clipboard-write; encrypted-media; picture-in-picture", allowFullScreen: true }),
+                React.createElement("h5", { className: "card-title" }, "Abstract"),
                 React.createElement("p", { className: "card-text" }, "Most tabular data visualization techniques focus on overviews, yet many practical analysis tasks are concerned with investigating individual items of interest. At the same time, relating an item to the rest of a potentially large table is important. In this work we present Taggle, a tabular visualization technique for exploring and presenting large and complex tables. Taggle takes an item-centric, spreadsheet-like approach, visualizing each row in the source data individually using visual encodings for the cells. At the same time, Taggle introduces data-driven aggregation of data subsets. The aggregation strategy is complemented by interaction methods tailored to answer specific analysis questions, such as sorting based on multiple columns and rich data selection and filtering capabilities. We demonstrate Taggle using a case study conducted by a domain expert on complex genomics data analysis for the purpose of drug discovery."),
-                React.createElement("p", { className: "card-text" }, "Taggle was published in the Information Visualization journal. Please consider a citation when using Taggle and publishing your results."),
-                React.createElement("p", { className: "card-text text-muted" },
+                React.createElement("h5", { className: "card-title" }, "Citation"),
+                React.createElement("p", { className: "card-text" },
                     "Katarina Furmanova, Samuel Gratzl, Holger Stitz, Thomas Zichner, Miroslava Jaresova, Martin Ennemoser, Alexander Lex, Marc Streit.",
                     React.createElement("br", null),
                     React.createElement("b", null, "Taggle: Combining Overview and Details in Tabular Data Visualizations."),
@@ -74,7 +76,8 @@ const TagglePublication = () => {
                 React.createElement("a", { href: "https://github.com/lineupjs/lineupjs", target: "_blank", rel: "noopener noreferrer", className: "btn btn-light" },
                     React.createElement("i", { className: "mr-1 fab fa-github" }),
                     " Source code"))));
-};
+}
+;
 const publications = [
     {
         id: 'ordino-publication',
@@ -102,12 +105,18 @@ export function PublicationPage() {
             React.createElement(OrdinoScrollspy, { items: publications.map((publication) => ({ id: publication.id, name: publication.name })) }, (handleOnChange) => React.createElement(React.Fragment, null,
                 React.createElement("div", { className: "ordino-publication-page container pb-6" },
                     React.createElement("div", { className: "row" },
+                        React.createElement("div", { className: "col" },
+                            React.createElement("p", { className: "lead text-ordino-gray-4 pt-6" },
+                                "Ordino and its components have been described in the following scientific publications.",
+                                React.createElement("br", null),
+                                "Please cite at least the first article when using Ordino and publishing your results."))),
+                    React.createElement("div", { className: "row" },
                         React.createElement("div", { className: "col" }, publications.map((item, index) => {
                             return (
                             // `id` attribute must match the one in the scrollspy
-                            React.createElement(OrdinoScrollspyItem, { className: "pt-6", id: item.id, key: item.name, index: index, handleOnChange: handleOnChange },
+                            React.createElement(OrdinoScrollspyItem, { className: (index === 0) ? 'pt-3' : 'pt-6', id: item.id, key: item.name, index: index, handleOnChange: handleOnChange },
                                 React.createElement(React.Fragment, null,
-                                    React.createElement("h4", { className: "text-left mt-2 mb-3" },
+                                    React.createElement("h5", { className: "text-left mt-2 mb-3" },
                                         React.createElement("i", { className: `mr-2 ordino-icon-2 ${item.icon}` }),
                                         " ",
                                         item.name),
