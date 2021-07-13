365f24d7a47373aeee5657753644a9ce683b8851
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2021-07-13 09:46:04 +0200

    use col

--- src/pages/components/GettingStarted.tsx
+++ src/pages/components/GettingStarted.tsx
@@ -9,15 +9,15 @@ export function GettingStarted() {
         </div>
       </div>
       <ol className="row ordino-getting-started-steps mx-0 mt-5 p-0">
-        <li className="col-12 col-lg mb-sm-5 mb-lg-0 text-center">
+        <li className="col col-lg mb-sm-5 mb-lg-0 text-center">
           <h2 className="my-3">Define list of entities</h2>
           <p>Select the genes, cell lines, or tissue samples from our predefined dataset or upload your own dataset of interest.</p>
         </li>
-        <li className="col-12 col-lg mb-sm-5 mb-lg-0 text-center">
+        <li className="col col-lg mb-sm-5 mb-lg-0 text-center">
           <h2 className="my-3">Rank, filter and select entities</h2>
           <p>Rank and filter the entities by different attributes. Add more data on demand. Select most interesting entities.</p>
         </li>
-        <li className="col-12 col-lg mb-sm-5 mb-lg-0 text-center">
+        <li className="col col-lg mb-sm-5 mb-lg-0 text-center">
           <h2 className="my-3">Obtain detailed information</h2>
           <p>Dive deeper into the selected entities and obtain further information or visualize selected entities.</p>
         </li>
